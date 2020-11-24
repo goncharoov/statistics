9abe58481a2ee2f25d64711c8d9603fcb1fa8126
goncharoov <takeyo69@icloud.com>
2020-11-24 17:32:57 +1000

    added test findMaxWithChangedData

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,18 @@ class StatisticsServiceTest {
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
-
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
+    assertEquals(expected, actual);
+  }
 
+  @Test
+  void findMaxWithChangedData() {
+    StatisticsService service = new StatisticsService();
+    long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 12;
+    long actual = service.findMax(incomesInBillions);
     assertEquals(expected, actual);
   }
 }
